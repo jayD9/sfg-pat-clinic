a5211b4fc9e3914cd4b716e9127b3d44f411275c
Jay Darji <jaydarji369@gmail.com>
2020-12-31 15:25:31 -0500

    Initial Project add

---
+++ src/test/java/jd/springframwork/sfgpatclinic/SfgPatClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package jd.springframwork.sfgpatclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPatClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
